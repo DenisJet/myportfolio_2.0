e0506f04ba06842aa8811e00b724844964b8b585
Denis Kalkopf <deniskalkopf@gmail.com>
2024-02-10 11:18:17 +0500

    add project

---
+++ src/app/portfolio/krossover/page.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+export default function Krossover(): JSX.Element {
+  return (
+    <main>
+      <div className='container main p-4 mx-auto max-w-4xl'>
+        <h1 className='gradient text-6xl font-bold mt-6 mb-5'>Krossover</h1>
+        <p className='font-light mb-4 text-lg'>Оn-line store.</p>
+        <img src='/projects/krossover.png' alt='site image' />
+        <p className='font-light mt-4 mb-4 text-lg'>
+          A site for selling shoes and clothing online. Created with <strong className='font-medium'>WordPress</strong>.
+        </p>
+        <p className='font-light mb-1 text-lg'>Additional resources:</p>
+        <ul>
+          <li>
+            <Link href='https://krossover-shoes.ru/' target='_blank' className='font-light hover:underline'>
+              - https://krossover-shoes.ru/
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,6 +1,21 @@
 export const projects = [
   {
-    id: '1',
+    id: '3',
+    link: '/portfolio/baths-restoration',
+    title: 'Baths restoration',
+    description: 'Semantic, adaptive layout, using tools such as Gulp, Pug, Bootstrap.',
+    year: 2023,
+    iconsSrc: [
+      '/icons/iconhtml.svg',
+      '/icons/iconcss.svg',
+      '/icons/icon-javascript.svg',
+      '/icons/icons-gulp.png',
+      '/icons/pug_icon.svg',
+      '/icons/icon-bootstrap.svg',
+    ],
+  },
+  {
+    id: '2',
     link: '/portfolio/cleanhouse',
     title: 'Clean House',
     description: 'Semantic, valid, adaptive, fluid, cross-browser website layout for the Clean House company.',
@@ -12,5 +27,13 @@ export const projects = [
       '/icons/iconsass.svg',
       '/icons/icons-gulp.png',
     ],
+  },
+  {
+    id: '1',
+    link: '/portfolio/krossover',
+    title: 'Krossover',
+    description: 'Оn-line store for selling shoes and clothing.',
+    year: 2022,
+    iconsSrc: ['/icons/iconhtml.svg', '/icons/iconcss.svg', '/icons/icon-wordpress.svg'],
   },
 ];
