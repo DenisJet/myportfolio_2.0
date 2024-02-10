810b8ca1d53e1a8a7713ab79dab115e3b8fb11bb
Denis Kalkopf <deniskalkopf@gmail.com>
2024-02-10 15:37:04 +0500

    fix

--- src/app/portfolio/blog-me/page.tsx
+++ src/app/portfolio/blog-me/page.tsx
@@ -4,7 +4,7 @@ export default function BlogMe(): JSX.Element {
   return (
     <main className=''>
       <div className='container main p-4 mx-auto max-w-4xl'>
-        <h1 className='gradient text-6xl font-bold mt-6 mb-5'>Blog Me</h1>
+        <h1 className='gradient text-6xl leading-tight font-bold mt-6 mb-5'>Blog Me</h1>
         <p className='font-light mb-4 text-lg'>
           One of my educational projects. Blog, with the ability to write articles.
         </p>
@@ -14,7 +14,10 @@ export default function BlogMe(): JSX.Element {
           implements user registration and authorization. Reading, creating, deleting, changing articles. Uploading
           images to the server. Possibility to leave comments, etc.
         </p>
-        <p className='font-light mt-1 mb-4 text-lg'>Implemented the ability to send a message about a call back.</p>
+        <p className='font-light mt-1 mb-4 text-lg'>
+          The project was created using{' '}
+          <strong>MongoDB, Express, React, Material Ui, Redux Toolkit, Axios, JWT, BCrypt, Multer</strong>...
+        </p>
         <p className='font-light mb-1 text-lg'>Additional resources:</p>
         <ul>
           <li>
