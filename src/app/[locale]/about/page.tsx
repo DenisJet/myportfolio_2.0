a9a40967e2ccae3144c941dbdbef0b48d0dfed3f
Denis Kalkopf <deniskalkopf@gmail.com>
2024-03-13 19:24:51 +0500

    fix

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -14,14 +14,14 @@ export default function About(): JSX.Element {
           <Link
             href='https://assets.htmlacademy.ru/certificates/profession/39/1875311.pdf?1665069782&_ga=2.193242148.86894964.1665994142-270059664.1655958253'
             target='_blank'
-            className='m-5 hover:opacity-100 opacity-75'
+            className='m-5'
           >
             <Image src='/front-sert.png' alt='' width={100} height={50} className='w-full h-auto' />
           </Link>
           <Link
             href='https://assets.htmlacademy.ru/certificates/profession/47/1875311.pdf?1692890714'
             target='_blank'
-            className='m-5 hover:opacity-100 opacity-75'
+            className='m-5'
           >
             <Image src='/react-sert.png' alt='' width={100} height={50} className='w-full h-auto' />
           </Link>
