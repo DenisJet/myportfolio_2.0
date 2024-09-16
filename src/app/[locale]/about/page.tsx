7f94a3b159af9b3acd395cd324dd3010c9d5a168
Denis Kalkopf <ddenproton@protonmail.com>
2024-09-16 20:57:12 +0500

    add experience ru

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -10,7 +10,7 @@ export default function About(): JSX.Element {
       <div className='container p-4 mx-auto max-w-4xl'>
         <h1 className='gradient text-6xl font-bold mt-6 mb-6'>{t('title')}</h1>
         <p className='font-light text-lg mb-3'>{t('text')}</p>
-        <div className='my-3 flex'>
+        {/* <div className='my-3 flex'>
           <Link
             href='https://assets.htmlacademy.ru/certificates/profession/39/1875311.pdf?1665069782&_ga=2.193242148.86894964.1665994142-270059664.1655958253'
             target='_blank'
@@ -25,7 +25,7 @@ export default function About(): JSX.Element {
           >
             <Image src='/react-sert.png' alt='' width={100} height={50} className='w-full h-auto' />
           </Link>
-        </div>
+        </div> */}
         <h2 className='text-xl font-semibold mb-4'>{t('title-2')}</h2>
         <div className='p-0 mt-2 flex items-start flex-wrap'>
           <div className='flex bg-slate-200 py-[3px] px-[15px] mr-2 mb-2 rounded space-x-2'>JavaScript</div>
---
+++ src/experience/experienceRu.ts
@@ -0,0 +1,64 @@
+export const experienceRu = [
+  {
+    id: '1',
+    date: 'Июнь 2024 — по настоящее время',
+    name: 'Bogoda Digital Pro',
+    links: ['https://bogoda.pro/', 'https://guider.pro/'],
+    position: 'Frontend-разработчик',
+    stack: ['TypeScript', 'Next.js', 'Redux Toolkit', 'SCSS', 'Git Flow', 'Rest API'],
+    description:
+      'Создание новых, адаптивных компонентов. Исправление багов и улучшение существующего кода, проведение код-ревью. Настройка работы поиска в приложении, работа с REST API, работа с картами - React-Leaflet API. Взаимодействие с дизайнерами и бэкенд-разработчиками.',
+  },
+  {
+    id: '2',
+    date: 'Июнь 2024 — Август 2024',
+    name: 'Antipoff Group',
+    links: ['https://antipoff.iamrobot.xyz/'],
+    position: 'Frontend-разработчик Junior, intern',
+    stack: ['React.js', 'Redux', 'React-router', 'TypeScript', 'Git Flow', 'SCSS', 'Rest API', 'GraphQL', 'MaterialUi'],
+    description:
+      'Принимал участие в разработке коммерческих проектов. Создание новых, адаптивных компонентов, правка существующих компонентов, устранение багов. Работа с Rest API, настройка запросов на бэкенд, с помощью GraphQL',
+  },
+  {
+    id: '3',
+    date: 'Август 2023 — Май 2024',
+    name: 'Индивидуальное предпринимательство / частная практика',
+    links: [],
+    position: 'Frontend-разработчик',
+    stack: [
+      'HTML',
+      'CSS',
+      'SCSS',
+      'JavaScript',
+      'TypeScript',
+      'React JS',
+      'Next JS',
+      'Redux Toolkit',
+      'Rest API',
+      'Gulp',
+      'Webpack',
+    ],
+    description:
+      'Создание отзывчивых и мобильно-адаптивных веб-страниц, приложений и компонентов, разработка пользовательских интерфейсов. Доработка и внесение правок, оптимизация производительности и доступности веб-страниц, сайтов и веб-приложений путем оптимизации кода.',
+  },
+  {
+    id: '4',
+    date: 'Ноябрь 2022 — Август 2023',
+    name: 'ООО «Интерактивные обучающие технологии»',
+    links: ['https://htmlacademy.ru/'],
+    position: 'React-разработчик (в рамках практической части курса)',
+    stack: ['React.js', 'Redux', 'React-router', 'SCSS', 'Rest API'],
+    description:
+      'Разработка веб-интерфейсов при помощи JavaScript. Разработка SPA на чистом JavaScript. Разработка веб-приложений на React',
+  },
+  {
+    id: '5',
+    date: 'Март 2022 — Сентябрь 2022',
+    name: 'Clean House',
+    links: ['Веб-разработчик'],
+    position: 'React-разработчик (в рамках практической части курса)',
+    stack: ['HTML', 'CSS', 'SCSS', 'TypeScript', 'JavaScript', 'БЭМ', 'GULP'],
+    description:
+      'Разработка и создание сайта с нуля, адаптивная, семантическая верстка. Регистрация домена, размещение сайта на хостинге. Наполнение контентом.',
+  },
+];
