a80aac0be939af6095168acd0b6b3c10e4a0f7a4
Denis Kalkopf <ddenproton@protonmail.com>
2024-09-16 21:20:37 +0500

    add experience en

---
+++ src/experience/experienceEn.ts
@@ -0,0 +1,64 @@
+export const experienceEn = [
+  {
+    id: '1',
+    date: 'June 2024 - Present',
+    name: 'Bogoda Digital Pro',
+    links: ['https://bogoda.pro/', 'https://guider.pro/'],
+    position: 'Frontend developer',
+    stack: ['TypeScript', 'Next.js', 'Redux Toolkit', 'SCSS', 'Git Flow', 'Rest API'],
+    description:
+      'Creating new, adaptive components. Fixing bugs and improving existing code, conducting code reviews. Setting up search in the application, working with REST API, working with maps - React-Leaflet API. Interacting with designers and backend developers.',
+  },
+  {
+    id: '2',
+    date: 'June 2024 - August 2024',
+    name: 'Antipoff Group',
+    links: ['https://antipoff.iamrobot.xyz/'],
+    position: 'Frontend developer Junior, intern',
+    stack: ['React.js', 'Redux', 'React-router', 'TypeScript', 'Git Flow', 'SCSS', 'Rest API', 'GraphQL', 'MaterialUi'],
+    description:
+      'Participated in the development of commercial projects. Creating new, adaptive components, editing existing components, eliminating bugs. Working with Rest API, setting up backend queries using GraphQL',
+  },
+  {
+    id: '3',
+    date: 'August 2023 - May 2024',
+    name: 'Sole proprietorship / private practice',
+    links: [],
+    position: 'Frontend developer',
+    stack: [
+      'HTML',
+      'CSS',
+      'SCSS',
+      'JavaScript',
+      'TypeScript',
+      'React JS',
+      'Next JS',
+      'Redux Toolkit',
+      'Rest API',
+      'Gulp',
+      'Webpack',
+    ],
+    description:
+      'Creating responsive and mobile-adaptable web pages, applications and components, developing user interfaces. Finalizing and making edits, optimizing the performance and accessibility of web pages, sites and web applications by optimizing the code.',
+  },
+  {
+    id: '4',
+    date: 'November 2022 - August 2023',
+    name: 'LLC "Interactive educational technologies"',
+    links: ['https://htmlacademy.ru/'],
+    position: 'React developer (as part of the practical part of the course)',
+    stack: ['React.js', 'Redux', 'React-router', 'SCSS', 'Rest API'],
+    description:
+      'Developing Web Interfaces with JavaScript. Developing SPAs with Pure JavaScript. Developing Web Applications with React',
+  },
+  {
+    id: '5',
+    date: 'March 2022 - September 2022',
+    name: 'Clean House',
+    links: ['https://cleanhouse-ses.ru/'],
+    position: 'React-разработчик (в рамках практической части курса)',
+    stack: ['HTML', 'CSS', 'SCSS', 'TypeScript', 'JavaScript', 'BEM', 'GULP'],
+    description:
+      'Development and creation of a website from scratch, adaptive, semantic layout. Domain registration, website placement on hosting. Filling with content.',
+  },
+];
